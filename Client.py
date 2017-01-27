745ff1b6fd25d18c00bab2aa7f3be0472b3a4fa2
Jonathan Bezeau <jbezeau@uvic.ca>
2017-01-27 10:44:05 -0800

    Added functionality to send multiple messages and then close chat when desired

--- Client.py
+++ Client.py
@@ -1,28 +1,27 @@
 print "This is the client"
 
 
-
-#The following is sample code from https://wiki.python.org/moin/TcpCommunication
-#I intend to use this code as a base to learn about basic socket programming in python
-
-
-#!/usr/bin/env python 
-
 import socket
 
 
 TCP_IP = raw_input("Enter the IP address (Use 127.0.0.1 for messaging self)")
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = raw_input("Enter the message")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
+while (True):
+	MESSAGE = raw_input("Enter the message")
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(MESSAGE)
+	
+	if (MESSAGE == "quit"): break
+	data = s.recv(BUFFER_SIZE)
+
+
 s.close()
 
-print "received data: ", data
+print "Connection terminated"
 
 
 
--- Server.py
+++ Server.py
@@ -1,29 +1,24 @@
 print "This is the server code"
 
 
-
-#The following is sample code from https://wiki.python.org/moin/TcpCommunication
-#I intend to use this code as a base to learn about basic socket programming in python
-
-
-#!/usr/bin/env python
-
 import socket
 
 
 TCP_IP = raw_input("Enter the IP address (Use 127.0.0.1 for messaging self)")
 TCP_PORT = 5005
-BUFFER_SIZE = 20 #Normally 1024, but we want a fast response
+BUFFER_SIZE = 50 #Normally 1024, but we want a fast response, 50 is the "character limit"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-conn, addr = s.accept()
-print "Connection address: ", addr
+
 while True:
+	conn, addr = s.accept()
+	print "Connection address: ", addr
 	data = conn.recv(BUFFER_SIZE)
 	if not data: break
+	if (data == "quit"): break
 	print "received data: ", data
 	conn.send(data) #echo
 conn.close()

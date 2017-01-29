af528796bc28a0cd1f3582d19f7c1d45fda15e03
Jonathan Bezeau <jonbezeau@gmail.com>
2017-01-28 18:12:39 -0800

    Made connecting to the same computer easier

--- Client.py
+++ Client.py
@@ -4,12 +4,15 @@ print "This is the client"
 import socket
 
 
-TCP_IP = raw_input("Enter the IP address (Use 127.0.0.1 for messaging self)")
+TCP_IP = raw_input("Enter the IP address of the server (empty string for messaging self)")
+if (TCP_IP == ""): TCP_IP = "127.0.0.1"
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 
+
 while (True):
 	MESSAGE = raw_input("Enter the message")
+	
 	
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((TCP_IP, TCP_PORT))
--- Server.py
+++ Server.py
@@ -4,7 +4,8 @@ print "This is the server code"
 import socket
 
 
-TCP_IP = raw_input("Enter the IP address (Use 127.0.0.1 for messaging self)")
+TCP_IP = raw_input("Enter your IP address (empty string for messaging self)")
+if (TCP_IP == ""): TCP_IP = "127.0.0.1"
 TCP_PORT = 5005
 BUFFER_SIZE = 1024 #This represents the character limit, just chose 1024 because it is a power of 2 and its quite large for my needs
 
